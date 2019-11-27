1e789af6d3b9334939295e1c547238c0a54a1806
Jean-Philippe Janecek <jpjanecek@imac-de-jean-philippe.home>
2019-11-27 23:44:41 +0100

    add influxdb, chronograf, telegraf, grafana

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,3 +4,9 @@ if [  -z "$(docker network ls|grep net-traefik)" ]; then
   docker network create net-traefik 
 fi
 docker-compose $@
+sleep 5
+docker exec grafana mkdir -p /etc/grafana/provisioning/dashboards
+docker exec grafana mkdir -p /etc/grafana/provisioning/datasources
+docker cp grafana/dashboard.yml grafana:/etc/grafana/provisioning/dashboards/
+docker cp grafana/datasource.yml grafana:/etc/grafana/provisioning/datasources/
+docker restart grafana
